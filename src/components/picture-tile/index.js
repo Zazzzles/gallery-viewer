5736066680162614f9e08dcd5617448325ec8b86
Ras Brits <rasbrits@yahoo.com>
2020-10-02 23:46:19 +0100

    Added eager loading

--- src/components/picture-tile/index.js
+++ src/components/picture-tile/index.js
@@ -6,13 +6,11 @@ import { Container, Maximized, Image } from './index.module.css';
 
 import NavigationContext from '../../context/navigation-context';
 
-// https://css-tricks.com/pre-caching-image-with-react-suspense/
-
 export default ({ urls }) => {
   const { xIndex } = useContext(NavigationContext);
   return (
     <div className={cn(Container, { [Maximized]: xIndex > 0 })}>
-      <img src={urls?.regular} className={Image} />
+      <img src={urls?.regular} loading='eager' className={Image} />
     </div>
   );
 };
